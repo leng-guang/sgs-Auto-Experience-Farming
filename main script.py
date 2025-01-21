a2520eef865864a947a347bdf89ec32d4b7a6d80
C_L1gh7 <m13811385457@163.com>
2025-01-21 23:27:17 +0800

    add ocr function

--- main script.py
+++ main script.py
@@ -4,21 +4,23 @@ import time
 from PIL import Image
 import numpy as np
 import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 def delay(x):
     for i in range(x):
         print(f"\r{x-i}", end='') #count down second
         time.sleep(1) #delay 1s
 
-def resizeimg(img, scale):
-    Template = Image.open(img)
+def resizeimg(path, scale):
+    Template = Image.open(path)
     width, height = Template.size
     NewSize = (int(width*scale), int(height*scale))
     resized_img = Template.resize(NewSize)
     return(resized_img)
 
 def BackToLobby():
-    time.sleep(5)
     return_img = resizeimg("pictures/return.png", scale)
     classical_img = resizeimg("pictures\classical.png", scale)
     # lobby_img = resizeimg("pictures/lobby.png", scale)
@@ -45,7 +47,7 @@ def BackToLobby():
                 print("\rCannot find return button", end='')
         else:
             returned = True
-            print("\r已返回大厅")
+            print("\r已返回大厅                           ")
 
 def apply_gamma_correction(image, gamma):
     """
@@ -74,116 +76,173 @@ def farming_matches(times):
             break
 
     for i in range(times):
-        while True:
-            try:
-                location = pa.locateCenterOnScreen(excercise2_img, region=region, confidence=0.8, grayscale=True)
-            except pa.ImageNotFoundException as e:
-                print(f"\rError: Cannot find exercise_image! {e}", end='')
-            else:
-                time.sleep(1)
-                pa.click(location)
-                print("\r已进入演练                             ")
-                break
-
-        # choose the character
-        HP_img = resizeimg("pictures\HP.png", scale)
-        region_character = (left, top+height//2, width*3//4, height//2)
-        time.sleep(9)
-        while True:
-            try:
-                time.sleep(1)
-                location = pa.locateCenterOnScreen(HP_img, region=region_character, confidence=0.8, grayscale=True)
-            except pa.ImageNotFoundException as e:
-                print(f"\rError: Cannot find HP_image! {e}", end='')
-            else:
-                x, y = location
-                character_img = pa.screenshot(region=(int(x), int(y-(90*scale)), int(100*scale), int(90*scale)))
-                pa.doubleClick(location)
-                break
-        time.sleep(1)
-
-        # Template match
-        template = np.array(character_img)
-        template = cv2.cvtColor(template, cv2.COLOR_RGB2BGR)
-
         gamma = 3
         finded_charater = False
         while not finded_charater:
-            screenshot = pa.screenshot(region=(left, top, width, height))
-            screenshot.save("pictures/screenshot.png")
-            screenshot = np.array(screenshot)
-            screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
-            screenshot = apply_gamma_correction(screenshot, gamma)
-            template = apply_gamma_correction(template, gamma)
-            template_height, template_width = template.shape[:2]
-            
-            template_height, template_width = template.shape[:2]
-            result = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)
-            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-            print(max_val)
-            if max_val >= 0.9:
-                x = max_loc[0]+left+template_width//2
-                y = max_loc[1]+top+template_height//2
-                pa.doubleClick(x, y)
-            print("已点选武将")     
-
-            # quit from the game
-            plus_img = resizeimg("pictures\plus.png", scale)
-            quit_img = resizeimg("pictures\quit.png", scale)
-            confirm_img = resizeimg("pictures\confirm.png", scale)
-            start_time = time.time()
-            while time.time()-start_time<2:
+            while True:
                 try:
-                    location = pa.locateCenterOnScreen(plus_img, region=region, confidence=0.8, grayscale=True)
+                    location = pa.locateCenterOnScreen(excercise2_img, region=region, confidence=0.8, grayscale=True)
                 except pa.ImageNotFoundException as e:
-                    print(f"\rError: Cannot find plus_image! {e}", end='')
+                    print(f"\rError: Cannot find exercise_image! {e}", end='')
                 else:
-                    finded_charater = True #loaced the plus button
+                    time.sleep(1)
                     pa.click(location)
-                    time.sleep(1.3)
+                    print("\r已进入演练                             ")
+                    break
 
+            # choose the character
+            HP_img = resizeimg("pictures\HP.png", scale)
+            region_character = (left, top+height//2, width*3//4, height//2)
+            time.sleep(9)
+            while True:
+                try:
+                    time.sleep(1)
+                    location = pa.locateCenterOnScreen(HP_img, region=region_character, confidence=0.8, grayscale=True)
+                except pa.ImageNotFoundException as e:
+                    print(f"\rError: Cannot find HP_image! {e}", end='')
+                else:
+                    x, y = location
+                    character_img = pa.screenshot(region=(int(x), int(y-(90*scale)), int(100*scale), int(90*scale)))
+                    pa.doubleClick(location)
+                    # break
+                    time.sleep(1)
+
+                    # Template match
+                    template = np.array(character_img)
+                    template = cv2.cvtColor(template, cv2.COLOR_RGB2BGR)
+
+                    screenshot = pa.screenshot(region=(left, top, width, height))
+                    screenshot.save("pictures/screenshot.png")
+                    screenshot = np.array(screenshot)
+                    screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
+                    screenshot = apply_gamma_correction(screenshot, gamma)
+                    template = apply_gamma_correction(template, gamma)
+                    template_height, template_width = template.shape[:2]
+                    
+                    template_height, template_width = template.shape[:2]
+                    result = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)
+                    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+                    print(max_val)
+                    if max_val >= 0.9:
+                        x = max_loc[0]+left+template_width//2
+                        y = max_loc[1]+top+template_height//2
+                        pa.doubleClick(x, y)
+                    print("已点选武将")     
+
+                    # quit from the game
+                    plus_img = resizeimg("pictures\plus.png", scale)
+                    quit_img = resizeimg("pictures\quit.png", scale)
+                    confirm_img = resizeimg("pictures\confirm.png", scale)
                     start_time = time.time()
-                    # find quit button
-                    while time.time()-start_time<2: 
+                    while time.time()-start_time<2:
                         try:
-                            location = pa.locateCenterOnScreen(quit_img, region=region, confidence=0.8, grayscale=True)
+                            location = pa.locateCenterOnScreen(plus_img, region=region, confidence=0.8, grayscale=True)
                         except pa.ImageNotFoundException as e:
-                            print(f"\rError: Cannot find quit_image! {e}", end='')
+                            print(f"\rError: Cannot find plus_image! {e}", end='')
                         else:
+                            finded_charater = True #loaced the plus button
                             pa.click(location)
+                            time.sleep(1.3)
+
                             start_time = time.time()
-                            # find confirm button
-                            while time.time()-start_time < 2:
+                            # find quit button
+                            while time.time()-start_time<2: 
                                 try:
-                                    location = pa.locateCenterOnScreen(confirm_img, region=region, confidence=0.8, grayscale=True)
+                                    location = pa.locateCenterOnScreen(quit_img, region=region, confidence=0.8, grayscale=True)
                                 except pa.ImageNotFoundException as e:
-                                    print(f"\rError: Cannot find confirm_image! {e}", end='')
+                                    print(f"\rError: Cannot find quit_image! {e}", end='')
                                 else:
                                     pa.click(location)
-                                    break # located confirm button
+                                    start_time = time.time()
+                                    # find confirm button
+                                    while time.time()-start_time < 2:
+                                        try:
+                                            location = pa.locateCenterOnScreen(confirm_img, region=region, confidence=0.8, grayscale=True)
+                                        except pa.ImageNotFoundException as e:
+                                            print(f"\rError: Cannot find confirm_image! {e}", end='')
+                                        else:
+                                            pa.click(location)
+                                            break # located confirm button
+                                    else:
+                                        continue
+                                    break # located quit button
                             else:
-                                continue
-                            break # located quit button
+                                print("\r无法找到\"退出按钮\"，正在返回上一步操作                    ", end='')
+                            break # located puls button
                     else:
-                        print("\r无法找到\"退出按钮\"，正在返回上一步操作                    ", end='')
-                    break # located puls button
-            else:
-                print("\r无法找到\'+\'，正在返回上一步操作                 ", end='')
-                continue
-        
-        print(f"\r已刷{i+1}次                   ")
-        # if (i+1)%20 == 0:
-        #     print("正在返回大厅以缓解卡顿                  ")
-        #     BackToLobby()
-        #     while True:
-        #         try:
-        #             location = pa.locateCenterOnScreen(excercise1_img, region=region, confidence=0.8, grayscale=True)
-        #         except pa.ImageNotFoundException as e:
-        #             print(f"\rError: Cannot find exercise_image! {e}", end='')
-        #         else:
-        #             pa.click(location)
-        #             print("\r已进入演练                             ")
-        #             break
+                        print("\r无法找到\"+\"，正在返回上一步操作                 ", end='')
+                        continue
+                    break
+        print(f"\r已刷{i+1}次                      ")
+        if ((i+1)%1 == 0) and (i+1)>=1:
+            print("正在返回大厅并检查历练值                  ")
+            BackToLobby()
+            spacialOffer_img = resizeimg("pictures\spacial_offer.png", scale)
+            activities_img = resizeimg("pictures\\activities.png", scale)
+            GalleryOfHeros_img = resizeimg("pictures\Gallery of Heroes.png", scale)
+            boost_img = resizeimg("pictures\\boost.png", scale)
+            while True:
+                try:
+                    location = pa.locateCenterOnScreen(spacialOffer_img, region=region, confidence=0.8, grayscale=True)
+                except pa.ImageNotFoundException as e:
+                    print(f"\rError: Cannot find spacial_offer_image! {e}", end='')
+                else:
+                    pa.click(location)
+                    break # located spacial offer button
+
+            while True:
+                try:
+                    location = pa.locateCenterOnScreen(activities_img, region=region, confidence=0.8, grayscale=True)
+                except pa.ImageNotFoundException as e:
+                    print(f"\rError: Cannot find activities_image! {e}", end='')
+                else:
+                    pa.click(location)
+                    break # located activities button
+
+            while True:
+                try:
+                    location = pa.locateCenterOnScreen(GalleryOfHeros_img, region=region, confidence=0.8, grayscale=True)
+                except pa.ImageNotFoundException as e:
+                    print(f"\rError: Cannot find Gallery of Heroes image! {e}", end='')
+                else:
+                    pa.click(location)
+                    break # located spacial offer button
+            
+            while True:
+                try:
+                    location = pa.locateOnScreen(boost_img, region=region, confidence=0.8, grayscale=True)
+                except pa.ImageNotFoundException as e:
+                    print(f"\rError: Cannot find boost image! {e}", end='')
+                else:
+                    num_left, num_top, num_weight, num_height = location
+                    location = (int(num_left), int(num_top-num_height), num_weight, num_height)
+                    print(location)
+                    num_img = pa.screenshot(region=location)
+                    num_img.save('num_img1.png')
+                    num_img = num_img.convert('L')
+                    threshold = 75
+                    num_img = num_img.point(lambda p: p>threshold and 255)
+                    num_img.save('num_img2.png')
+                    # num_img = np.array(num_img)
+                    number = pytesseract.image_to_string(num_img)
+                    print(number)
+                    break # located boost button
+
+            if number.strip() == "60/60":
+                print("已刷满群英绘墙，正在返回大厅")
+                BackToLobby()
+                return
+            
+            BackToLobby()
+            while True:
+                try:
+                    location = pa.locateCenterOnScreen(classical_img, region=region, confidence=0.8, grayscale=True)
+                except pa.ImageNotFoundException as e:
+                    print(f"\rError: Cannot find classical_image! {e}", end='')
+                else:
+                    pa.click(location)
+                    print("\r已进入演练                             ")
+                    break
 
 
 #########################################################################################################
@@ -209,7 +268,7 @@ while SizeCheck:
         print("\r窗口大小满足条件")
 
 # calculate the scale factor
-scale = width/1600
+scale = height/934
 print(f"窗口分辨率为：{width, height}缩放大小为{scale}")
 
 # # start game
